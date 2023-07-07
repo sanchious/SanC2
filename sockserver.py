9f079cb35a5488d0e1049b366edd5e51e258996d
Alex M <sasha_midor@yahoo.com>
2023-07-06 20:17:05 -0400

    Initial Server & Client commit

---
+++ sockserver.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+
+
+def listener_handler():
+    sock.bind((host_ip, host_port))
+    print('[+] Awaiting connection from clinet...')
+    sock.listen()
+    remote_target, remote_ip = sock.accept()
+    print(f'[+] Connection received from {remote_ip[0]}')
+    while True:
+        try:
+            message = input('Message to send#> ')
+            print('[+] Awating response...')
+            if message == 'exit':
+                remote_target.send(message.encode())
+                remote_target.close()
+                print('[-] Closing the connection...')
+                break
+            remote_target.send(message.encode())
+            response = remote_target.recv(1024).decode()
+            if response == 'exit':
+                print('[-] The client has terminated the session.')
+                remote_target.close()
+                break
+            print(response)
+        except KeyboardInterrupt:
+            remote_target.close()
+            print('[+] Keyboard interrupt issued')
+            break
+        except Exception:
+            remote_target.close()
+            break
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = sys.argv[1]
+host_port = int(sys.argv[2])
+listener_handler()
